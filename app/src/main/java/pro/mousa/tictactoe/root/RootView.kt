06745ca058f8ad13395d7f0451aeb272208fb12e
Mousa Moradi <mousamk@gmail.com>
2018-12-11 22:58:03 +0330

    RootView is a subclass of FrameLayout now.

--- app/src/main/java/pro/mousa/tictactoe/root/RootView.kt
+++ app/src/main/java/pro/mousa/tictactoe/root/RootView.kt
@@ -2,7 +2,7 @@ package pro.mousa.tictactoe.root
 
 import android.content.Context
 import android.util.AttributeSet
-import android.view.View
+import android.widget.FrameLayout
 
 
 /**
@@ -11,4 +11,4 @@ import android.view.View
 class RootView @JvmOverloads constructor(context: Context,
                                          attrs: AttributeSet? = null,
                                          defStyle: Int = 0)
-    : View(context, attrs, defStyle), RootInteractor.RootPresenter
+    : FrameLayout(context, attrs, defStyle), RootInteractor.RootPresenter
